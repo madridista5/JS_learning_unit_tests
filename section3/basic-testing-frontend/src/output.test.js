3a72f2bb0d6c4984aef09016a1e8cae1e20fb6d7
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-27 11:05:18 +0100

    Function generateResultText has been tested.

---
+++ section3/basic-testing-frontend/src/output.test.js
@@ -0,0 +1,17 @@
+import {generateResultText} from './output.js';
+
+describe('generateResultText()', () => {
+    it('should return a string, no matter which value is passed in', () => {
+        const val1 = 1;
+        const val2 = true;
+        const val3 = [];
+
+        const result1 = generateResultText(val1);
+        const result2 = generateResultText(val2);
+        const result3 = generateResultText(val3);
+
+        expect(result1).toBeTypeOf('string');
+        expect(result2).toBeTypeOf('string');
+        expect(result3).toBeTypeOf('string');
+    });
+});
